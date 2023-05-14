eb626de6a5ec24bb5c30d2578eb568d330c473cd
Nutchi <nutchinet@outlook.com>
2023-05-14 14:55:48 +0900

    feat: フデの塗るアルゴリズムを変更

--- src/main/java/net/nutchi/splatoonmc/util/Fude.java
+++ src/main/java/net/nutchi/splatoonmc/util/Fude.java
@@ -10,33 +10,52 @@ import net.nutchi.splatoonmc.SplatoonMC;
 
 public class Fude {
     public static void fillMyColor(SplatoonMC plugin, Player player) {
-        Location loc = player.getEyeLocation();
-        Vector dir = loc.getDirection();
-        Location base = loc.add(dir.multiply(3));
+        String coolTimeObject = "rollercool";
 
-        int distance = 2;
+        if (ScoreboardUtil.getScore(plugin, coolTimeObject, player.getName()) <= 0) {
+            Location loc = player.getEyeLocation();
+            Vector dir = loc.getDirection();
 
-        int fromX = base.getBlockX() - distance;
-        int fromY = base.getBlockY() - distance;
-        int fromZ = base.getBlockZ() - distance;
-        int toX = base.getBlockX() + distance;
-        int toY = base.getBlockY() + distance;
-        int toZ = base.getBlockZ() + distance;
+            for (int i = 2; i <= 5; i++) {
+                Location base = loc.clone().add(dir.clone().multiply(i));
+                Block baseBlock = base.getBlock();
 
-        for (int x = fromX; x <= toX; x++) {
-            for (int y = fromY; y <= toY; y++) {
-                for (int z = fromZ; z <= toZ; z++) {
-                    Block block = base.getWorld().getBlockAt(x, y, z);
-                    getYourBlockMaterial(plugin, player.getName()).ifPresent(yourMaterial -> {
-                        getMyBlockMaterial(plugin, player.getName()).ifPresent(myMaterial -> {
-                            if (block.getType().equals(yourMaterial)
-                                    || block.getType().equals(Material.POLISHED_DEEPSLATE)) {
-                                block.setType(myMaterial);
-                            }
-                        });
-                    });
+                int fromX = baseBlock.getX() - 1;
+                int fromY = baseBlock.getY() - 3;
+                int fromZ = baseBlock.getZ() - 1;
+                int toX = baseBlock.getX() + 1;
+                int toY = baseBlock.getY() + 1;
+                int toZ = baseBlock.getZ() + 1;
+
+                int smallX = fromX <= toX ? fromX : toX;
+                int smallY = fromY <= toY ? fromY : toY;
+                int smallZ = fromZ <= toZ ? fromZ : toZ;
+                int bigX = fromX >= toX ? fromX : toX;
+                int bigY = fromY >= toY ? fromY : toY;
+                int bigZ = fromZ >= toZ ? fromZ : toZ;
+
+                for (int x = smallX; x <= bigX; x++) {
+                    for (int y = smallY; y <= bigY; y++) {
+                        for (int z = smallZ; z <= bigZ; z++) {
+                            Block block = base.getWorld().getBlockAt(x, y, z);
+                            getYourBlockMaterial(plugin, player.getName())
+                                    .ifPresent(yourMaterial -> {
+                                        getMyBlockMaterial(plugin, player.getName())
+                                                .ifPresent(myMaterial -> {
+                                                    if (block.getType().equals(yourMaterial)
+                                                            || block.getType().equals(
+                                                                    Material.POLISHED_DEEPSLATE)) {
+                                                        block.setType(myMaterial);
+                                                    }
+                                                });
+                                    });
+                        }
+                    }
                 }
+
             }
+
+            ScoreboardUtil.setScore(plugin, coolTimeObject, player.getName(), 20);
         }
     }
 
